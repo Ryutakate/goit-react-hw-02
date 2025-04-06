538b04e3ec3c820c56801ff3b91f629334db0d82
Ryutakate <magegavladislav@gmail.com>
2025-04-06 23:53:17 +0300

    add src components and app.jsx v-3

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: '/goit-react-hw-02/', 
   json: {
-    stringify: true, 
+    stringify: true,
   },
 });
