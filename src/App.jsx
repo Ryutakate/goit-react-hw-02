35462c2892bba622bd097e6367250160a532131c
Ryutakate <magegavladislav@gmail.com>
2025-04-07 16:13:05 +0300

    add src components and app.jsx v-5

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import Feedback from './components/Feedback/Feedback';
 import Options from './components/Options/Options';
 import Notification from './components/Notification/Notification';
-import Description from './components/Description/Description'; // ✅ імпорт
+import Description from './components/Description/Description';
 
 const App = () => {
   const [feedbacks, setFeedbacks] = useState(() => {
